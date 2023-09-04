71931d4ff80f0098fa3daed5170f5bcc79bb8d2e
Zeycan <zeycanaslan7@gmail.com>
2023-09-04 14:03:40 +0300

    add new project

---
+++ FootballAnthem/Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace FootballAnthem
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(0, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(263, 192);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(262, 0);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(263, 192);
+            this.button2.TabIndex = 1;
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(525, 0);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(263, 192);
+            this.button3.TabIndex = 2;
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(0, 198);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(263, 192);
+            this.button4.TabIndex = 3;
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(262, 198);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(263, 192);
+            this.button5.TabIndex = 4;
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(525, 198);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(263, 192);
+            this.button6.TabIndex = 5;
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(14, 407);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(131, 20);
+            this.checkBox1.TabIndex = 6;
+            this.checkBox1.Text = "FENERBAHÇE";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(161, 407);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(102, 20);
+            this.checkBox2.TabIndex = 7;
+            this.checkBox2.Text = "BEŞİKTAŞ";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(269, 407);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.checkBox3.Size = new System.Drawing.Size(139, 20);
+            this.checkBox3.TabIndex = 8;
+            this.checkBox3.Text = "GALATASARAY";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            this.checkBox3.CheckedChanged += new System.EventHandler(this.checkBox3_CheckedChanged);
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.checkBox4.Location = new System.Drawing.Point(414, 407);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(143, 20);
+            this.checkBox4.TabIndex = 9;
+            this.checkBox4.Text = "TRABZONSPOR";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            this.checkBox4.CheckedChanged += new System.EventHandler(this.checkBox4_CheckedChanged);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(497, 426);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(15, 16);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "0\r\n";
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(563, 396);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(213, 46);
+            this.axWindowsMediaPlayer1.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(788, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.axWindowsMediaPlayer1);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ FootballAnthem/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FootballAnthem
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        int second = 0;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            second = Convert.ToInt32(label1.Text);
+            second++;
+            label1.Text = second.ToString();
+
+            void renksec(string ilk, string ikinci) {
+                if (second % 10 == 0)
+                {
+                    button1.BackColor = Color.FromName(ilk);
+                    button2.BackColor = Color.FromName(ikinci);
+                    button3.BackColor = Color.FromName(ilk);
+                    button4.BackColor = Color.FromName(ikinci);
+                    button5.BackColor = Color.FromName(ilk);
+                    button6.BackColor = Color.FromName(ikinci);
+                }
+
+                if (second % 10 == 5)
+                {
+                    button1.BackColor = Color.FromName(ikinci);
+                    button2.BackColor = Color.FromName(ilk);
+                    button3.BackColor = Color.FromName(ikinci);
+                    button4.BackColor = Color.FromName(ilk);
+                    button5.BackColor = Color.FromName(ikinci);
+                    button6.BackColor = Color.FromName(ilk);
+                }
+            }
+            if (checkBox1.Checked == true)
+            {
+                renksec("Yellow", "Blue");
+                checkBox2.Checked = false;
+                checkBox3.Checked = false;
+                checkBox4.Checked = false;
+            }
+
+            if (checkBox2.Checked == true)
+            {
+                renksec("Black", "White");
+                checkBox1.Checked = false;
+                checkBox3.Checked = false;
+                checkBox4.Checked = false;
+            }
+
+            if (checkBox3.Checked == true)
+            {
+                renksec("Yellow", "Red");
+                checkBox1.Checked = false;
+                checkBox2.Checked = false;
+                checkBox4.Checked = false;
+            }
+
+            if (checkBox4.Checked == true)
+            {
+                renksec("Red", "Blue");
+                checkBox1.Checked = false;
+                checkBox2.Checked = false;
+                checkBox3.Checked = false;
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+            axWindowsMediaPlayer1.URL = "C:\\Users\\USER\\Desktop\\GUTHUBDESKTOP\\C# PROJECTS\\FootballAnthem\\ANTHEM\\100-yil-sarkisi-official-video.mp3";
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+            axWindowsMediaPlayer1.URL = "C:\\Users\\USER\\Desktop\\GUTHUBDESKTOP\\C# PROJECTS\\FootballAnthem\\ANTHEM\\gucune-guc-katmaya-geldik-besiktas-official-video.mp3";
+        }
+
+        private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+            axWindowsMediaPlayer1.URL = "C:\\Users\\USER\\Desktop\\GUTHUBDESKTOP\\C# PROJECTS\\FootballAnthem\\ANTHEM\\galatasaray-marsi.mp3";
+        }
+
+        private void checkBox4_CheckedChanged(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+            axWindowsMediaPlayer1.URL = "C:\\Users\\USER\\Desktop\\GUTHUBDESKTOP\\C# PROJECTS\\FootballAnthem\\ANTHEM\\Biz Dar Sokaklarında.mp3";
+        }
+    }
+}
